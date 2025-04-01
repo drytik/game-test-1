0f5cd113d820eb0134766a87d939d846f8d11411
drytik <logicadri@gmail.com>
2025-04-01 21:51:32 +0200

    Day5
    Progress in game scene subviewport and camera.

--- scenes/singletons/interface/interface.gd
+++ scenes/singletons/interface/interface.gd
@@ -1,10 +1,15 @@
 class_name Interface
 extends Control
 
-#TODO MAKE SINGLETON 
+#TODO MAKE SINGLETON, consider moving to TabContainer node
 
 @onready var tab_container : TabContainer = $MarginContainer/HBoxContainer/TabContainer
 
 func _ready() -> void:
-	#tab_container.tab_changed.connect(on_tab_changed)
-	pass
+	tab_container.tab_changed.connect(_on_tab_changed)
+	
+func _on_tab_changed(tab_idx : int) -> void: 
+	var current_tab :  = tab_container.get_child(tab_idx)
+	if current_tab.name == "MainScene":
+		var player = current_tab.get_node("SubViewportContainer/SubViewport/World/PlayerScene")
+		player.camera.make_current()
