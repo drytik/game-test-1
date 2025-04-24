01a9c05f05a21b902c7006e9e142ca86cd025344
drytik <logicadri@gmail.com>
2025-04-24 21:25:15 +0300

    Day11
    Market panel work in progress.

--- scenes/earth_scene/earth_scene.gd
+++ scenes/earth_scene/earth_scene.gd
@@ -1,10 +1,10 @@
 extends Control
 
-@onready var label : Label = $MarginContainer/TabContainer/CityMenu/TradePanel/VBoxContainer/MarginContainer/VBoxContainer/MarginContainer3/HBoxContainer/MarginContainer4/RichTextLabel
+#@onready var label : Label = $MarginContainer/TabContainer/CityMenu/TradePanel/VBoxContainer/MarginContainer/VBoxContainer/MarginContainer3/HBoxContainer/MarginContainer4/RichTextLabel
 
-func _ready() -> void:
-	SeedsGenerator.ref.produced.connect(_update_label)
-	label.text = "0"
+#func _ready() -> void:
+#	SeedsGenerator.ref.produced.connect(_update_label)
+	#label.text = "0"
 	
-func _update_label() -> void: 
-	label.text = "%s" %Game.ref.data.resources.seeds
+#func _update_label() -> void: 
+	#label.text = "%s" %Game.ref.data.resources.seeds
